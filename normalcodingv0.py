6dd7dbd361f1ddee6eb430a43dc56c7a9c125e14
A.C Holding <84363801+ACHolding@users.noreply.github.com>
2026-06-10 13:46:19 -0700

    Add files via upload

---
+++ normalcodingv0.py
@@ -0,0 +1,14 @@
+# this is  a test
+print(" I LOVE CODING" )
+import os 
+import sys 
+print("WOW ")
+
+
+# 
+
+a = ("Enter a number")
+print(a)
+b = input("Here ya go enter the 2nd No. $ ")
+print(b)
+input("Press Enter to exit...")
